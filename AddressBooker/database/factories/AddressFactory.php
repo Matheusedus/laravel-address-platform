c98c24910f6629752177498cc895a8f0a4bbf0a7
matheusedus <matheuslega@hotmail.com.br>
2025-06-26 23:42:16 -0300

    chore: adiciona factories, seeders e model

---
+++ AddressBooker/database/factories/AddressFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Address>
+ */
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => \App\Models\User::factory(),
+            'cep' => $this->faker->postcode(),
+            'logradouro' => $this->faker->streetName(),
+            'bairro' => $this->faker->citySuffix(),
+            'cidade' => $this->faker->city(),
+            'estado' => $this->faker->stateAbbr(),
+        ];
+    }
+}
